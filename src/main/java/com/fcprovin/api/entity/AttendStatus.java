a0a390408fef32bffaa68ef74bbadef281bfc469
discphy <dinophy@nate.com>
2023-03-06 20:10:08 +0900

    feat(add): EnumType for common code

--- src/main/java/com/fcprovin/api/entity/AttendStatus.java
+++ src/main/java/com/fcprovin/api/entity/AttendStatus.java
@@ -1,5 +1,21 @@
 package com.fcprovin.api.entity;
 
-public enum AttendStatus {
-    ATTEND, LATE, ABSENT, REASON
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum AttendStatus implements EnumType {
+
+    ATTEND("출석"),
+    LATE("지각"),
+    ABSENT("결석"),
+    REASON("사유");
+
+    private final String description;
+
+    @Override
+    public String getName() {
+        return name();
+    }
 }
--- src/main/java/com/fcprovin/api/entity/SnsType.java
+++ src/main/java/com/fcprovin/api/entity/SnsType.java
@@ -1,5 +1,21 @@
 package com.fcprovin.api.entity;
 
-public enum SnsType {
-    GOOGLE, APPLE, NAVER, KAKAO
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum SnsType implements EnumType {
+
+    GOOGLE("구글"),
+    APPLE("애플"),
+    NAVER("네이버"),
+    KAKAO("카카오");
+
+    private final String description;
+
+    @Override
+    public String getName() {
+        return name();
+    }
 }
