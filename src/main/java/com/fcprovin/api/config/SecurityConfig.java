0bb3e8f4dee6669e7ea4518bf2d399ced0e1fd3b
discphy <dinophy@nate.com>
2023-03-15 22:14:18 +0900

    feat(add): Security config added endpoint matchers for permit all

--- src/main/java/com/fcprovin/api/config/SecurityConfig.java
+++ src/main/java/com/fcprovin/api/config/SecurityConfig.java
@@ -10,8 +10,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static com.fcprovin.api.config.jwt.JwtRole.ROLE_ADMIN;
-import static com.fcprovin.api.config.jwt.JwtRole.ROLE_USER;
+import static com.fcprovin.api.dto.jwt.JwtRole.ROLE_ADMIN;
+import static com.fcprovin.api.dto.jwt.JwtRole.ROLE_USER;
 import static org.springframework.http.HttpMethod.POST;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
@@ -31,7 +31,7 @@ public class SecurityConfig {
                 .httpBasic().disable()
                 .sessionManagement().sessionCreationPolicy(STATELESS).and()
                 .authorizeRequests()
-                    .antMatchers(POST, "/api/**/auth/**", "/api/**/sns", "/api/**/member").permitAll()
+                    .antMatchers(POST, "/api/**/auth/access-token", "/api/**/sns", "/api/**/member").permitAll()
                     .anyRequest().hasAnyRole(ROLE_USER.getName(), ROLE_ADMIN.getName())
                     .and()
                 .addFilterBefore(new JwtAuthenticationFilter(provider), UsernamePasswordAuthenticationFilter.class)
