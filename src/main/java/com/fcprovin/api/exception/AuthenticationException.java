c5cc02c2ab0cd582f650ae2420c2a85c1a3ac641
discphy <dinophy@nate.com>
2023-03-26 17:13:41 +0900

    feat(add): SecurityConfig exception handler process added

---
+++ src/main/java/com/fcprovin/api/exception/AuthenticationException.java
@@ -0,0 +1,20 @@
+package com.fcprovin.api.exception;
+
+public class AuthenticationException extends RuntimeException {
+
+    public AuthenticationException() {
+        super();
+    }
+
+    public AuthenticationException(String message) {
+        super(message);
+    }
+
+    public AuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AuthenticationException(Throwable cause) {
+        super(cause);
+    }
+}
