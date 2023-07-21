02a69199df6d8c0b01d65c15dbf2cbec97808b61
discphy <dinophy@nate.com>
2023-07-21 11:23:53 +0700

    feat(add): Match repository added

--- src/main/java/com/fcprovin/api/repository/MatchRepositoryImpl.java
+++ src/main/java/com/fcprovin/api/repository/MatchRepositoryImpl.java
@@ -2,11 +2,13 @@ package com.fcprovin.api.repository;
 
 import com.fcprovin.api.dto.search.MatchSearch;
 import com.fcprovin.api.entity.Match;
+import com.fcprovin.api.entity.MatchStatus;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -31,7 +33,52 @@ public class MatchRepositoryImpl implements MatchQueryRepository {
                 .where(
                         teamIdEqual(search.getTeamId()),
                         stadiumIdEqual(search.getStadiumId()),
+                        statusEqual(search.getStatus()),
                         matchDateBetween(search.getStartDate(), search.getEndDate())
+                ).fetch();
+    }
+
+    @Override
+    public List<Match> findQueryVoteStartByStatus(MatchStatus status) {
+        return queryFactory
+                .selectFrom(match)
+                .join(match.team, team).fetchJoin()
+                .where(
+                        statusEqual(status),
+                        beforeVoteStartDate()
+                ).fetch();
+    }
+
+    @Override
+    public List<Match> findQueryVoteEndByStatus(MatchStatus status) {
+        return queryFactory
+                .selectFrom(match)
+                .join(match.team, team).fetchJoin()
+                .where(
+                        statusEqual(status),
+                        beforeVoteEndDate()
+                ).fetch();
+    }
+
+    @Override
+    public List<Match> findQueryMatchStartByStatus(MatchStatus status) {
+        return queryFactory
+                .selectFrom(match)
+                .join(match.team, team).fetchJoin()
+                .where(
+                        statusEqual(status),
+                        beforeMatchStartDate()
+                ).fetch();
+    }
+
+    @Override
+    public List<Match> findQueryMatchEndByStatus(MatchStatus status) {
+        return queryFactory
+                .selectFrom(match)
+                .join(match.team, team).fetchJoin()
+                .where(
+                        statusEqual(status),
+                        beforeMatchEndDate()
                 ).fetch();
     }
 
@@ -45,12 +92,32 @@ public class MatchRepositoryImpl implements MatchQueryRepository {
                 .fetchOne());
     }
 
+    private BooleanExpression beforeVoteStartDate() {
+        return match.voteDate.startDate.before(LocalDateTime.now());
+    }
+
+    private BooleanExpression beforeVoteEndDate() {
+        return match.voteDate.endDate.before(LocalDateTime.now());
+    }
+
+    private BooleanExpression beforeMatchEndDate() {
+        return match.matchDate.endDate.before(LocalDateTime.now());
+    }
+
+    private BooleanExpression beforeMatchStartDate() {
+        return match.matchDate.startDate.before(LocalDateTime.now());
+    }
+
     private BooleanExpression teamIdEqual(Long id) {
         return nonNull(id) ? match.team.id.eq(id) : null;
     }
 
     private BooleanExpression stadiumIdEqual(Long id) {
         return nonNull(id) ? match.stadium.id.eq(id) : null;
+    }
+
+    private BooleanExpression statusEqual(MatchStatus status) {
+        return nonNull(status) ? match.status.eq(status) : null;
     }
 
     private BooleanExpression matchDateBetween(LocalDate startDate, LocalDate endDate) {
