7adcf0243f251ca8cd191f978aafbe91b7fbf6ca
discphy <dinophy@nate.com>
2024-02-12 13:31:06 +0900

    fix(update): dto package update

--- src/main/java/com/fcprovin/api/dto/search/PlayerSearch.java
+++ src/main/java/com/fcprovin/api/dto/search/PlayerSearch.java
@@ -3,11 +3,15 @@ package com.fcprovin.api.dto.search;
 import com.fcprovin.api.entity.BaseStatus;
 import com.fcprovin.api.entity.PlayerAuthority;
 import com.fcprovin.api.entity.Position;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PlayerSearch {
 
     private Long memberId;
--- src/main/java/com/fcprovin/api/dto/search/TeamSearch.java
+++ src/main/java/com/fcprovin/api/dto/search/TeamSearch.java
@@ -1,11 +1,15 @@
 package com.fcprovin.api.dto.search;
 
 import com.fcprovin.api.entity.BaseStatus;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TeamSearch {
 
     private Long regionId;
